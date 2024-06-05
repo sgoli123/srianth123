65ec1ed34ca5d442a478a21c4c4a88ab48917e1b
sgoli123 <golisrinathreddy@gmail.com>
2024-06-05 20:51:37 +1000

    Create sp.sql

---
+++ sp.sql
@@ -0,0 +1,13 @@
+// Define a base class 'Animal' which encapsulates common properties and methods
+class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
